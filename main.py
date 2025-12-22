80a2cee0befad498dc5cd37974fbe8457fa9b5f7
Canxmax <temugearincan@gmail.com>
2025-12-22 22:11:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "ROFL": "bir şakaya karşılık cevap",
-            "SHEESH": "onaylamamak",
-            "CREEPY": "korkunç",
-            "AGGRO":  "agresifleşmek/sinirlenmek",
-            }
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-for i in range(5):
-    if word in meme_dict.keys():
-        # Kelime eşleşiyorsa ne yapmalıyız?
-        print(meme_dict[word])
-    else:
-        print("böyle bir kelime yokkkkkkkk")
-        # Kelime eşleşmiyorsa ne yapmalıyız?
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+bot.run("")
